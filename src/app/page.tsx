79ec3688da6c21e27532499d6704aa14a31e45dc
matthns <matthns98@gmail.com>
2023-07-21 14:48:17 -0300

    home page changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,15 @@
-import Image from "next/image";
-import Link from "next/link";
-import Welcome from "./pages/Welcome";
 import { NavBar } from "./organisms/NavBar";
 
-const LogoImg = () => {
-  return (
-    <Image
-      src="/assets/logo.png"
-      className=" ml-5"
-      alt="MMA Devs logo"
-      width={200}
-      height={50}
-    />
-  );
-};
+import Welcome from "./pages/Welcome";
 
-const Menu = () => {
-  return (
-    <div className="flex justify-center  text-white">
-      {/* text-sky-600 */}
-      {/* transition duration-300 border-b-8 border-transparent hover:border-sky-500 */}
-      <Link
-        href={"/"}
-        className="p-3 ml-2 mr-2 hover:text-sky-600 active:font-bold"
-      >
-        HOME
-      </Link>
-      <Link
-        href={"#"}
-        className="p-3 ml-2 mr-2  hover:text-sky-600 active:font-bold"
-      >
-        SOBRE
-      </Link>
-      <Link
-        href={"#"}
-        className="p-3 ml-2 mr-2  hover:text-sky-600 active:font-bold"
-      >
-        PORTFÓLIO
-      </Link>
-      <Link
-        href={"#"}
-        className="p-3 ml-2 mr-2  hover:text-sky-600 active:font-bold"
-      >
-        CONTATO
-      </Link>
-    </div>
-  );
-};
-
-export default function Home() {
+export default function App() {
   return (
     <>
-      <main className="bg-dark-blue w-screen h-screen text-white">
+      <div className="h-screen w-screen">
         <NavBar />
         <Welcome />
-      </main>
+        <h1>Teste</h1>
+      </div>
     </>
   );
 }
--- src/app/pages/Welcome.tsx
+++ src/app/pages/Welcome.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Script from "next/script";
 
 const ProjectImg = () => {
   return (
@@ -6,8 +7,8 @@ const ProjectImg = () => {
       src="/assets/image-1.png"
       className=""
       alt="Projeto"
-      width={500}
-      height={250}
+      width={600}
+      height={480}
     />
   );
 };
@@ -15,20 +16,25 @@ const ProjectImg = () => {
 const Welcome = () => {
   return (
     <>
-      <div className="flex justify-center">
-        <div>
-          <p className="mt-24 w-[35rem] text-5xl font-bold text-sky-600">
-            A solução para o seu negócio.
-          </p>
-          <p className="mt-5 w-[32rem] text-white">
-            Entregamos o que você precisa, de forma ágil, eficaz e com valor
-            justo, que cabe no seu bolso. Faça hoje mesmo um orçamento conosco!
-          </p>
-          <button className="mt-8 rounded-lg p-3 bg-blue-500 hover:bg-blue-800">
-            Solicitar orçamento
-          </button>
+      <div className="bg-[url('/assets/img/bg.jpg')] bg-cover z-0">
+        <div className="w-screen h-screen flex justify-center items-center   text-white bg-dark-blue/90 z-10">
+          {/* bg-dark-blue/90 */}
+
+          <div className="box-border h-80 w-[40rem] p-4">
+            <p className="text-5xl font-bold text-sky-400">
+              A solução para o seu negócio.
+            </p>
+            <p className="mt-5">
+              Entregamos o que você precisa, de forma ágil, eficaz e com valor
+              justo, que cabe no seu bolso. Faça hoje mesmo um orçamento
+              conosco!
+            </p>
+            <button className="rounded-lg mt-5 p-3 text-white bg-blue-800 hover:bg-blue-600">
+              Solicitar orçamento
+            </button>
+          </div>
+          <ProjectImg />
         </div>
-        <ProjectImg />
       </div>
     </>
   );
