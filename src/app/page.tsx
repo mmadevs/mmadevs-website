dd0187c418c9ca132dfe06cf6d524e8e72bf60e3
Matheus Nascimento <matthns98@gmail.com>
2023-08-09 18:16:27 -0300

    fix wrong imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import NavBarProvider from "./context/navbarContext";
-import Footer from "./organisms/Footer";
-import { NavBar } from "./organisms/NavBar";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Projects from "./pages/Projects";
-import Welcome from "./pages/Welcome";
+import Footer from "@/app/components/organisms/Footer";
+import { NavBar } from "@/app/components/organisms/NavBar";
+import About from "@/app/components/pages/About";
+import Contact from "@/app/components/pages/Contact";
+import Projects from "@/app/components/pages/Projects";
+import Welcome from "@/app/components/pages/Welcome";
 
 export default function App() {
   return (
