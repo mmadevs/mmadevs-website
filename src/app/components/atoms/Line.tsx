c6442528b36aebd8d5809809b30cb76c560b9c3d
MMA Devs <mmadevsys@gmail.com>
2023-08-19 23:03:55 -0300

    feat: added animation to line

--- src/app/components/atoms/Line.tsx
+++ src/app/components/atoms/Line.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export const Line = () => (
-  <div className="relative min-h-[6px] w-full">
+  <div className="relative min-h-[6px] w-full  animate-ping animate-infinite animate-alternate-reverse animate-duration-[1.5s]">
     <Image src={"/assets/img/line.svg"} alt="breaking line" fill />
   </div>
 );
