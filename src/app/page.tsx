8e8a2be4dca21840f3d83dfbf2662f797b3a1b30
Anthony Victor <thonnyvrc@gmail.com>
2023-08-20 14:25:05 -0300

    feat: finished form with emailjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,23 +7,28 @@ import Contact from "@/app/components/pages/Contact";
 import Projects from "@/app/components/pages/Projects";
 import Welcome from "@/app/components/pages/Welcome";
 import { Line } from "./components/atoms/Line";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App() {
   return (
-    <NavBarProvider>
-      <div
-        className="bg-black relative w-full max-w-full h-auto min-h-screen 
+    <>
+      <NavBarProvider>
+        <div
+          className="bg-black relative w-full max-w-full h-auto min-h-screen 
       overflow-x-hidden overflow-y-auto"
-      >
-        <NavBar />
-        <Welcome />
-        <Line />
-        <About />
-        <Projects />
-        <Line />
-        <Contact />
-        {/* <Footer /> */}
-      </div>
-    </NavBarProvider>
+        >
+          <NavBar />
+          <Welcome />
+          <Line />
+          <About />
+          <Projects />
+          <Line />
+          <Contact />
+          {/* <Footer /> */}
+        </div>
+      </NavBarProvider>
+      <ToastContainer position="bottom-right" hideProgressBar theme="dark" />
+    </>
   );
 }
